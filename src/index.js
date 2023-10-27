c8244dd9d27eff5616f4f3f7914e0ff993ffdd63
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-10-28 04:07:10 +0800

    [ui][Adrian] site 50% done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ $(function () {
 
   let currentPage = 0;
   let posts;
-  let user;
+  let user = JSON.parse(localStorage.getItem("user"));
 
   async function showToast(message, isError = false) {
     const toastId = `#toast`;
@@ -21,6 +21,63 @@ $(function () {
     }, 1000);
   }
 
+  async function deletePost(id) {
+    try {
+      if (!user) throw new Error("You must be authenticated to delete a post!");
+
+      $.ajax({
+        method: "POST",
+        url: `${DOMAIN_NAME}/forumDeletePost.php?id=${id}`,
+        success: (data) => {
+          data = JSON.parse(data);
+          if (!data && !data.success)
+            throw new Error("Post deletion failed somehow...");
+
+          posts = posts.filter((post) => post.id !== id);
+          displayPosts();
+          showToast("Post deleted successfully!");
+        },
+      });
+    } catch (err) {
+      showToast(`${err.message}`, true);
+    }
+  }
+
+  async function displayPosts() {
+    try {
+      const postsContainer = $("#posts-container");
+      const start = POSTS_PER_PAGE * currentPage;
+      const end = Math.min(posts.length, start + POSTS_PER_PAGE);
+      let html = "";
+
+      for (let i = start; i < end; ++i) {
+        html += `
+            <div
+              class="px-2 w-full break-words overflow-hidden py-4 border-t border-white/20 hover:bg-black/20 transition-all flex justify-between"
+            >
+              <div class="w-[90%]">
+                <h3 class="text-xl font-bold">${posts[i].user}</h3>
+                <p class="text-xs">${posts[i].date}</p>
+                <p class="mt-2">${posts[i].post}</p>
+              </div>
+              <button
+                class="delete-button p-1 transition-all hover:bg-white/20 rounded-lg mx-auto w-fit h-fit hover:text-red-300"
+                value="${posts[i].id}"
+              >⌫
+              </button>
+            </div>
+            `;
+      }
+
+      postsContainer.html(html);
+      $(".delete-button").on("click", (e) => {
+        deletePost($(e.target).attr("value"));
+      });
+    } catch (error) {
+      showToast(`${error.message}`);
+    }
+  }
+
   async function getPosts() {
     try {
       $.ajax({
@@ -28,29 +85,22 @@ $(function () {
         url: `${DOMAIN_NAME}/forumGetPosts.php`,
         success: (data) => {
           posts = JSON.parse(data);
-          posts.sort((a, b) => new Date(b).getTime() - new Date(a).getTime());
-          const postsContainer = $("#posts-container");
-
-          const start = POSTS_PER_PAGE * currentPage;
-          const end = Math.min(posts.length, start + POSTS_PER_PAGE);
-          let html = "";
-
-          for (let i = start; i < end; ++i) {
-            html += `
-              <div class="px-2 py-4 border-t border-white/20 hover:bg-black/20 transition-all">
-                <h3 class="text-xl font-bold">${posts[i].user}</h3>
-                <p class="text-sm">${posts[i].date}</p>
-                <p class="">${posts[i].post}</p>
-              </div>
-            `;
-          }
-
-          postsContainer.html(html);
+          posts = posts.reverse();
+          displayPosts();
         },
       });
     } catch (err) {
       showToast(`${err.message}`, true);
     }
+  }
+
+  async function setUser(newUser = undefined) {
+    if (newUser) {
+      localStorage.setItem("user", JSON.stringify(newUser));
+      user = newUser;
+    }
+
+    $("#username-header").text(`${user.username}`);
   }
 
   async function createUser() {
@@ -76,6 +126,8 @@ $(function () {
         data: inputData,
         success: (data) => {
           user = JSON.parse(data);
+
+          setUser(user);
           showToast(`User created successfully!
           Welcome ${user.username}`);
         },
@@ -91,30 +143,33 @@ $(function () {
     try {
       if (!user) throw new Error("You must be authenticated to create a post!");
 
-      const text = $("#text").val();
-      const id = user.id;
+      const post = $("#text").val();
+      const id = JSON.stringify(user.id);
 
-      if (text.length === 0) throw new Error("Post must not be empty!");
-      if (text.length > MAX_CHARACTERS_ON_TEXT)
+      if (post.length === 0) throw new Error("Post must not be empty!");
+      if (post.length > MAX_CHARACTERS_ON_TEXT)
         throw new Error(
           `Post's character length must not exceed ${MAX_CHARACTERS_ON_TEXT}!`
         );
 
       $.ajax({
         method: "POST",
-        url: `${DOMAIN_NAME}/forumCreateUser.php`,
-        data: { text, id },
+        url: `${DOMAIN_NAME}/forumNewPost.php`,
+        data: { post, id },
         success: (data) => {
-          if (!data) throw new Error("Post failed somehow...");
+          data = JSON.parse(data);
+
+          if (!data && !data.success)
+            throw new Error("Post creation failed somehow...");
           else showToast("Post created successfully!");
+
+          getPosts();
         },
       });
     } catch (err) {
       showToast(`${err.message}`, true);
     }
   }
-
-  async function deletePost() {}
 
   async function replyPost() {}
 
@@ -131,4 +186,6 @@ $(function () {
   $("#create-post").on("click", createPost);
   $("#toggle-auth-form").on("click", handleToggleClass);
   getPosts();
+
+  if (user) setUser();
 });
